c90f7f7c035eb85fa1db1f682586500f13f2a67a
tichise <ichise@pepabo.com>
2019-03-29 15:30:22 +0900

    rewritten with optional binding

--- Sources/PreviewViewController.swift
+++ Sources/PreviewViewController.swift
@@ -14,12 +14,18 @@ open class PreviewViewController: UIViewController {
         super.viewDidLoad()
         
         self.title = previewPageTitle
-                
-        webView?.scrollView.decelerationRate = UIScrollView.DecelerationRate.normal
-        
-        let replacedHtml: String = html!.replacingOccurrences(of: "\n", with: "<br/>")
-        let htmlString: String = "<html><meta name='viewport' content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;'></head><body><div style='padding:10px;line-height:1.5em'>\(replacedHtml)</div></body></html>"
-        webView?.loadHTMLString(htmlString, baseURL: nil)
+
+        guard let webView = webView else {
+            return
+        }
+
+        webView.scrollView.decelerationRate = UIScrollView.DecelerationRate.normal
+
+        if let html = html {
+            let replacedHtml = html.replacingOccurrences(of: "\n", with: "<br/>")
+            let htmlString = "<html><meta name='viewport' content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;'></head><body><div style='padding:10px;line-height:1.5em'>\(replacedHtml)</div></body></html>"
+            webView.loadHTMLString(htmlString, baseURL: nil)
+        }
     }
 
     open func setHtml(_ html:String) {
